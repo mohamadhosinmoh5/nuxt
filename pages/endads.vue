7e7fd9477d3f5292b4ffd9fc082c1649ea66159f
Mohammad Mahdi <111296448+GodEye2004@users.noreply.github.com>
2024-04-17 10:24:51 -0800

    update-29

--- pages/endads.vue
+++ pages/endads.vue
@@ -18,14 +18,48 @@
                     </div>
                     <p class="mt-5 ms-2">عکس ها</p>
                     <p class="ms-2">اگهی های دارای عکس تا 3 برار بیشتر توسط کاربران دیده می شوند</p>
-                    <div class="addPicture">
+                    <!-- <div class="addPicture">
                         <a href="#" class="picture">افزودن عکس</a>
+                    </div> -->
+                    <!-- <dropzone/> -->
+                    <div @dragover.prevent @drop.prevent="drop" class="form">
+                        <div class="dropzone">
+                            <span typeof="file">drag or drop file</span>
+                            <span>OR</span>
+                            <input multiple type="file" id="dropzoneFile">
+                        </div>
+
+                        <!-- <div v-if="drop" class="showing">
+                            dsjcjdab
+                        </div> -->
                     </div>
+                    <!-- <span v-if="drop()" class="file-info">File: {{  }}</span> -->
                     <button type="button" class="col-2 next mt-5">
-                        <a href="/endads" class="nextPage">ثبت</a>
+                        <a href="" class="nextPage">ثبت</a>
                     </button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+
+<script setup>
+
+const drag = (e) => {
+
+    console.log(e);
+}
+const drop = (e) => {
+    let formData = new FormData();
+
+    const files = e.dataTransfer.files;
+    for (let index = 0; index < files.length; index++) {
+        const file = files[index];
+        formData.append(file);
+    }
+
+    console.log(files);
+
+}
+</script>
