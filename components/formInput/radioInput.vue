deae0a22586304d8ac824082876e4d2cae0ca289
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-04-28 11:50:15 +0330

    Create radioInput.vue

---
+++ components/formInput/radioInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-group">
+    <div class="form-check">
+      <input
+        :class="['form-check-input', inputClass]"
+        :type="type"
+        :id="inputId"
+        :v-model="modelValue"
+      />
+      <label :class="['form-check-label', labelClass]" :for="inputId">{{
+        label
+      }}</label>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const { label, inputId, modelValue, type, inputClass, labelClass } =
+  defineProps([
+    "label",
+    "inputId",
+    "modelValue",
+    "type",
+    "inputClass",
+    "labelClass",
+  ]);
+</script>
