5374bbc7222641e3256f3d1de33abf74c8468072
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-04-28 11:50:13 +0330

    Create select.vue

---
+++ components/formInput/select.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form-group">
+    <label :for="selectId">{{ label }}</label>
+    <select class="form-control" :id="selectId">
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const { label, selectId, options } = defineProps([
+  "label",
+  "selectId",
+  "options",
+]);
+</script>
