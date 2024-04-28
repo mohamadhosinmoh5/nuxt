489b4fa2194b5e4815b568ce4d400e79ceef2034
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-04-28 11:50:17 +0330

    Create number.vue

---
+++ components/formInput/number.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form-group">
+    <label :for="inputId">{{ label }}:</label>
+    <input
+      :type="inputType"
+      class="form-control"
+      :id="inputId"
+      :name="inputName"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const {
+  label,
+  inputId,
+  inputType = "number",
+  inputName = "",
+} = defineProps(["label", "inputId", "inputType", "inputName"]);
+</script>
