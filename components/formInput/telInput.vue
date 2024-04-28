4e2ea02e0df72395b17bb779402f7af8af08d50c
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-04-28 11:50:10 +0330

    Create telInput.vue

---
+++ components/formInput/telInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="form-group">
+    <label :for="inputId">{{ label }}</label>
+    <input
+      type="tel"
+      :class="['form-control', { 'is-invalid': error }]"
+      :id="inputId"
+      :placeholder="placeholder"
+      :v-model="modelValue"
+    />
+    <div v-if="error" class="invalid-feedback">
+      {{ error }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from "vue";
+
+const props = defineProps({
+  label: String,
+  inputId: String,
+  placeholder: String,
+  modelValue: String,
+  error: String,
+});
+</script>
