b6e711dd86fe4fadfb162e2c8fad4c6cadb03718
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-05-02 13:50:48 +0330

    Update textInput.vue

--- components/formInput/textInput.vue
+++ components/formInput/textInput.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="form-group">
     <label :for="inputId">{{ label }}</label>
-    <input :type="inputType" class="form-control" :id="inputId" :name="inputName"/>
+    <input
+      :type="inputType"
+      class="form-control"
+      :id="inputId"
+      :name="inputName"
+      @input="handleInput"
+    />
   </div>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
+import validate from "~/utils/validation";
 
 const {
   label,
   inputId,
   inputType = "text",
-  inputName=""
-} = defineProps(["label", "inputId", "inputType","name"]);
+  inputName = "",
+  validations = [],
+} = defineProps(["label", "inputId", "inputType", "inputName", "validations"]);
+
+const handleInput = (event) => {
+  const value = event.target.value;
+  const result = validate(value, validations);
+  console.log(result);
+  if (result === true) {
+    console.log("Valid input");
+  } else {
+    console.log(result.join("\n"));
+  }
+};
 </script>
