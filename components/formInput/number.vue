3d71146ad55ec5103f62aef97fd12bfbe35b5d96
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-05-05 11:14:22 +0330

    Update number.vue

--- components/formInput/number.vue
+++ components/formInput/number.vue
@@ -6,17 +6,36 @@
       class="form-control"
       :id="inputId"
       :name="inputName"
+      @input="handleInput"
     />
+    <span v-if="error" class="error-message">{{ error }}</span>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
+import validate from "~/utils/validation";
 
 const {
   label,
   inputId,
   inputType = "number",
   inputName = "",
-} = defineProps(["label", "inputId", "inputType", "inputName"]);
+  validations = [],
+} = defineProps(["label", "inputId", "inputType", "inputName" , "validations"]);
+
+const value = ref("");
+const error = ref("");
+
+const handleInput = (event) => {
+  value.value = event.target.value;
+  const result = validate(value.value, validations);
+  error.value = Array.isArray(result) ? result.join("\n") : "";
+};
 </script>
+
+<style>
+.error-message {
+  color: red;
+}
+</style>
